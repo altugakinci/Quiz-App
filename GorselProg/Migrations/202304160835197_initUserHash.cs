779e95146e83228ea8296624f62b8c60a56edcea
sefa onder <ondersefa111@gmail.com>
2023-04-16 12:02:47 +0300

    add hash field and handle login form

---
+++ GorselProg/Migrations/202304160835197_initUserHash.cs
@@ -0,0 +1,18 @@
+﻿namespace GorselProg.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initUserHash : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "salt", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "salt");
+        }
+    }
+}
