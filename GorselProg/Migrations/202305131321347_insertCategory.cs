ce4829a8b207f637dc27a136fed66e60440d8851
sefa onder <ondersefa111@gmail.com>
2023-05-13 21:37:04 +0300

    insertCategory over migration

---
+++ GorselProg/Migrations/202305131321347_insertCategory.cs
@@ -0,0 +1,22 @@
+﻿namespace GorselProg.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class insertCategory : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Categories (Name) VALUES ('Spor')");
+            Sql("INSERT INTO Categories (Name) VALUES ('Tarih')");
+            Sql("INSERT INTO Categories (Name) VALUES ('Sanat')");
+            Sql("INSERT INTO Categories (Name) VALUES ('Bilim')");
+            Sql("INSERT INTO Categories (Name) VALUES ('Eğlence')");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM Categories WHERE Name IN ('Spor', 'Tarih', 'Sanat', 'Bilim', 'Eğlence')");
+        }
+    }
+}
