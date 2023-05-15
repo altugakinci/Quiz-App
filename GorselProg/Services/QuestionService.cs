bb95fd716da1fd56816621e5f889dda8af840663
sefa onder <ondersefa111@gmail.com>
2023-05-15 16:54:13 +0300

    add QuestionService

--- GorselProg/Services/QuestionService.cs
+++ GorselProg/Services/QuestionService.cs
@@ -11,7 +11,139 @@ namespace GorselProg.Services
 
     class QuestionService
     {
-       
+        public static bool loadingIndicator = false;
+
+
+        public static void ShowLoadingIndicator()
+        {
+            loadingIndicator = true;
+        }
+        public static void HideLoadingIndicator()
+        {
+            loadingIndicator = true;
+        }
+        public static async Task<List<Question>> GetAllQuestions()
+        {
+            try
+            {
+                ShowLoadingIndicator();
+                using (var db = new qAppDBContext())
+                {
+                    var questions = await db.Questions.ToListAsync();
+                    return questions;
+                }
+            }
+            finally
+            {
+                HideLoadingIndicator();
+            }
+        }
+
+        public static async Task<Question> GetQuestionById(Guid id)
+        {
+            try
+            {
+                ShowLoadingIndicator();
+                using (var db = new qAppDBContext())
+                {
+                    var question = await db.Questions.FirstOrDefaultAsync(q => q.Id == id);
+                    return question;
+                }
+            }
+            finally
+            {
+                HideLoadingIndicator();
+            }
+        }
+
+        public static async Task<bool> AddQuestion(Question question)
+        {
+            try
+            {
+                ShowLoadingIndicator();
+                using (var db = new qAppDBContext())
+                {
+                    db.Questions.Add(question);
+                    await db.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                HideLoadingIndicator();
+            }
+        }
+
+        public static async Task<bool> UpdateQuestion(Question question)
+        {
+            try
+            {
+                ShowLoadingIndicator();
+                using (var db = new qAppDBContext())
+                {
+                    db.Entry(question).State = EntityState.Modified;
+                    await db.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                HideLoadingIndicator();
+            }
+        }
+
+        public static async Task<bool> DeleteQuestion(Guid id)
+        {
+            try
+            {
+                ShowLoadingIndicator();
+                using (var db = new qAppDBContext())
+                {
+                    var question = await db.Questions.FirstOrDefaultAsync(q => q.Id == id);
+                    if (question != null)
+                    {
+                        db.Questions.Remove(question);
+                        await db.SaveChangesAsync();
+                    }
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                HideLoadingIndicator();
+            }
+        }
+
+        public static async Task<List<Question>> GetQuestionsByCategory(Guid categoryId)
+        {
+            try
+            {
+                ShowLoadingIndicator();
+                using (var db = new qAppDBContext())
+                {
+                    var questions = await db.Questions
+                        .Where(q => q.CategoryId == categoryId)
+                        .ToListAsync();
+                    return questions;
+                }
+            }
+            finally
+            {
+                HideLoadingIndicator();
+            }
+        }
     }
 }
 
