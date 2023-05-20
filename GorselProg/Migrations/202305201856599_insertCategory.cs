5f85f70026bf9af38fc3098fad46f245cf8da80f
sefa onder <ondersefa111@gmail.com>
2023-05-20 22:56:51 +0300

    bug fixed

--- GorselProg/Migrations/202305201856599_insertCategory.cs
+++ GorselProg/Migrations/202305201856599_insertCategory.cs
@@ -10,18 +10,18 @@
             // Veri eklemek istediğiniz örnekleri oluşturunuz
             var categories = new[]
             {
-            new { Id = Guid.NewGuid(), Name = "Spor",Index = 0 },
-            new { Id = Guid.NewGuid(), Name = "Tarih",Index = 1 },
-            new { Id = Guid.NewGuid(), Name = "Sanat",Index = 2 },
-            new { Id = Guid.NewGuid(), Name = "Bilim",Index = 3 },
-            new { Id = Guid.NewGuid(), Name = "Eğlence",Index = 4 },
+            new { Id = Guid.NewGuid(), Name = "Spor", Index = 0 },
+            new { Id = Guid.NewGuid(), Name = "Tarih", Index = 1 },
+            new { Id = Guid.NewGuid(), Name = "Sanat", Index = 2 },
+            new { Id = Guid.NewGuid(), Name = "Bilim", Index = 3 },
+            new { Id = Guid.NewGuid(), Name = "Eğlence", Index = 4 },
 
         };
 
             // Kategorileri Category tablosuna ekleyin
             foreach (var category in categories)
             {
-                Sql($"INSERT INTO Category (Id, Name) VALUES ('{category.Id}', '{category.Name}')");
+                Sql($"INSERT INTO Category (Id,Name,[Index]) VALUES ('{category.Id}','{category.Name}','{category.Index}')");
             }
         }
 
